210eaaa8bfbda80a991f540e2b4b6df96d6c416d
Ryanwolff14 <ryan.wolff14@outlook.com>
2016-04-13 15:28:01 -0500

    Update payroll.py

--- payroll.py
+++ payroll.py
@@ -3,7 +3,7 @@ def hours():
 
     rate = float(input('how much do you make per hour?'))
 
-    if hours<40:
+    if hours<=40:
         print("The employee's pay is",(rate*hours))
     if hours>40:
         print("the employee's pay is",((hours-40)*(rate*1.5)+40*rate))
